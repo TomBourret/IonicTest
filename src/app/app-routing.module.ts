01b7a36fe85e5150bb691a8f8a1d0870fcd7443e
Tom <tom.bourret@insa-lyon.fr>
2019-05-01 12:37:22 +0200

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'test1', loadChildren: './pages/test1/test1.module#Test1PageModule' },
+  { path: 'test2', loadChildren: './pages/test2/test2.module#Test2PageModule' },
 ];
 
 @NgModule({
